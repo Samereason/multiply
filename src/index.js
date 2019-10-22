a0a4a5b36c110eff1f80842c7c2629dd3dfccf71
Yurii Shem <asreil1994@gmail.com>
2019-10-22 21:51:38 +0300

    use another's solution and edit a little

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function multiply(first, second) {
   // your solution
-}
+	const intOneArr = first.split('').reverse();
+	const intTwoArr = second.split('').reverse();
+
+	const stack = [];
+
+	for (let i = 0; i < intOneArr.length; i++) {
+		for (let j = 0; j < intTwoArr.length; j++) {
+			const m = intOneArr[i] * intTwoArr[j];
+			stack[i + j] = (stack[i + j]) ? stack[i + j] + m : m;
+		}
+	}
+
+	for (let i = 0; i < stack.length; i++) {
+		const num = stack[i] % 10;
+		const move = Math.floor(stack[i] / 10);
+
+		stack[i] = num;
+
+		if (stack[i + 1]) {
+			stack[i + 1] += move;
+		} else if (move !== 0) {
+			stack[i + 1] = move;
+		}
+	}
+
+	return stack.reverse().join('');
+};
